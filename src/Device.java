5c4459a8a7fdb170c87dac2b425a81b626facd96
sidya82 <quentin.rouland.82@gmail.com>
2015-01-07 11:57:19 +0100

    \o/

---
+++ src/Device.java
@@ -0,0 +1,17 @@
+import java.net.URL;
+
+/**
+ * Created by sidya on 07/01/15.
+ */
+public class Device {
+    private String deviceType;
+    private String friendlyName;
+    private URL manufactuerURL;
+    private String modelDescription;
+    private String modelName;
+    private String modelUrl;
+    private String serialNumber;
+    private String urlBase;
+    private URL presentationUrl;
+    private URL descriptionUrl;
+}
---
+++ src/Service.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+
+/**
+ * Created by sidya on 07/01/15.
+ */
+public class Service {
+    private String serviceType;
+    private String serviceID;
+    private URL controlURL;
+    private URL eventSubUrl;
+    private URL descriptionURL;
+}
